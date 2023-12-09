a0c20253cfbc2db2a615d10ea5bf0496cf534377
agnel077 <146045722+agnel077@users.noreply.github.com>
2023-12-08 20:42:56 -0500

    Add files via upload

---
+++ Php final project 1/footer.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="./css/style.css">
+<footer>
+    <div>
+        <!-- Your footer content goes here -->
+        &copy; <?php echo date("Y"); ?> Coco Cola Canada. All Rights Reserved.
+    </div>
+</footer>
+</body>
+</html>
---
+++ Php final project 1/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coca Cola</title>
+    <link rel="stylesheet" href="./css/style.css">
+    
+</head>
+<body>
+<!-- Header Section -->
+<header>
+    <!-- Your Logo Goes Here -->
+    <img src="./img/cola.png" alt="Your Logo">
+    <h1>Coca Cola</h1>
+</header>
+
+<!-- Navigation Section -->
+<nav>
+    <div class="nav-container">
+        <!-- Your Site Navigation Menu Goes Here -->
+        <ul>
+            <li><a href="index.php" class="home-link">Home</a></li>
+            <li><a href="register.php" class="register-link">Register</a></li>
+            <!-- Add more navigation links as needed -->
+        </ul>
+    </div>
+</nav>
